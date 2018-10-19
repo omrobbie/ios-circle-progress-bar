40f6659765a8fe74c6c024682f783825858c4241
omrobbie <putu.roby@yahoo.com>
2018-10-19 17:01:20 +0700

    Initial Commit

---
+++ Circle Progress Bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Circle Progress Bar
+//
+//  Created by omrobbie on 19/10/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
